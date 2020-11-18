a7d71386942c952e64c43fa09db40bd67d543c74
Sergey Kirillov <s_kirillov@rambler.ru>
2020-11-18 16:14:37 +0300

    add arguments && format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,14 @@ import program from 'commander';
 program
   .version('1.1.1')
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+  });
 
-// program.parse(process.argv);
+program.parse(process.argv);
+
+// if (program.format) console.log('- output format');
 // program.help();
