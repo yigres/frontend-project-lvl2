46191dc3fd4c5e347d3c0907e2ee944a9490c402
Sergey Kirillov <s_kirillov@rambler.ru>
2020-11-23 14:03:05 +0300

    add fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@jest/globals';
 import gendiff from '../index';
 
-const path1 = 'files/file1.json';
-const path2 = 'files/file2.json';
+const path1 = '__fixtures__/file1.json';
+const path2 = '__fixtures__/file2.json';
 const diff = '{\n  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n}\n';
 test('generate difference two json files', () => {
   expect(gendiff(path1, path2)).toBe(diff);
