6a1938ba9d715ad2d893953f8ce101e67a0143ef
Sergey Kirillov <s_kirillov@rambler.ru>
2020-11-23 15:02:45 +0300

    add __dirname && __filename

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,22 @@
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
 import gendiff from '../index';
 
-const path1 = '__fixtures__/file1.json';
-const path2 = '__fixtures__/file2.json';
-const diff = '{\n  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n}\n';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getStringResult = () => {
+  const readJsonFile = fs.readFileSync(getFixturePath('expected_file.json'));
+  const stringFile = `{${Object.entries(JSON.parse(readJsonFile)).reduce((acc, [a, b]) => `${acc}\n${a}: ${b}`, '')}\n}\n`;
+  return stringFile;
+};
+
+const path1 = getFixturePath('file1.json');
+const path2 = getFixturePath('file2.json');
+
 test('generate difference two json files', () => {
-  expect(gendiff(path1, path2)).toBe(diff);
+  expect(gendiff(path1, path2)).toBe(getStringResult());
 });
