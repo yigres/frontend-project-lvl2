2bc8f96dda954e63418239cc334f0e1295bd0cec
Sergey Kirillov <s_kirillov@rambler.ru>
2020-11-23 15:22:39 +0300

    refactoring genDiff

--- src/index.js
+++ src/index.js
@@ -2,33 +2,36 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
+const getDiff = (object1, object2) => {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  return `{\n${keys1.reduce((acc, key) => {
+    // ключа нет во втором файле
+    if (_.findIndex(keys2, (el) => el === key) === (-1)) {
+      return `${acc}- ${key}: ${object1[key]}\n`;
+    }
+    // значение ключа совпадают в обих файлах
+    if (object1[key] === object2[key]) {
+      return `${acc}  ${key}: ${object1[key]}\n`;
+    }
+    // значеие ключа отличается в файлах
+    return `${acc}- ${key}: ${object1[key]}\n+ ${key}: ${object2[key]}\n`;
+  }, '')}${keys2.reduce((acc, key) => {
+    // ключа нет в первом файле
+    if (_.findIndex(keys1, (el) => el === key) === (-1)) {
+      return `${acc}+ ${key}: ${object2[key]}\n`;
+    }
+    return acc;
+  }, '')}}\n`;
+};
+
 const gendiff = (filepath1, filepath2) => {
   const path1 = path.resolve(process.cwd(), filepath1);
   const path2 = path.resolve(process.cwd(), filepath2);
   try {
     const object1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
     const object2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
-    const keys1 = Object.keys(object1);
-    const keys2 = Object.keys(object2);
-    const diff = `{\n${keys1.reduce((acc, key) => {
-      // ключа нет во втором файле
-      if (_.findIndex(keys2, (el) => el === key) === (-1)) {
-        return `${acc}- ${key}: ${object1[key]}\n`;
-      }
-      // значение ключа совпадают в обих файлах
-      if (object1[key] === object2[key]) {
-        return `${acc}  ${key}: ${object1[key]}\n`;
-      }
-      // значеие ключа отличается в файлах
-      return `${acc}- ${key}: ${object1[key]}\n+ ${key}: ${object2[key]}\n`;
-    }, '')}${keys2.reduce((acc, key) => {
-      // ключа нет в первом файле
-      if (_.findIndex(keys1, (el) => el === key) === (-1)) {
-        return `${acc}+ ${key}: ${object2[key]}\n`;
-      }
-      return acc;
-    }, '')}}\n`;
-    return diff;
+    return getDiff(object1, object2);
   } catch (e) {
     console.log('что-то пошло не так!');
   }
