bb742815af2f50e6398b246d0412910539bc6f14
Sergey Kirillov <s_kirillov@rambler.ru>
2020-12-12 20:21:25 +0300

    second refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import gendiff from '../index.js';
 
 program
-  .version('1.1.1')
+  .version('1.1.2')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
--- src/index.js
+++ src/index.js
@@ -4,46 +4,55 @@ import path from 'path';
 import yaml from 'js-yaml';
 import format from './formatters/index.js';
 
-const getFileFullPath = (pathFile) => {
-  const fullPath = pathFile[0] === '/'
-    ? pathFile
-    : path.resolve(process.cwd(), pathFile);
-  return fullPath;
-};
-
-const getObject = (fullPathFile) => {
-  const data = fs.readFileSync(fullPathFile, 'utf8');
-  const fileFormat = path.extname(fullPathFile);
+const parseData = (data, fileFormat) => {
   // Выбирается функция-парсер в зависимости от расширения файла
   if (fileFormat === '.yml') {
     return yaml.safeLoad(data);
   }
-  if (fileFormat === '.json') {
-    return JSON.parse(data);
-  }
-  throw new Error('Files of this type are not supported!!!');
+
+  return JSON.parse(data);
 };
 
 const makeAst = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+  const keysOfObject1 = Object.keys(object1);
+  const keysOfObject2 = Object.keys(object2);
+  const keys = _.union(keysOfObject1, keysOfObject2);
   const result = {};
 
-  keys1.forEach((key) => {
-    if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+  keys.forEach((key) => {
+    if (
+      object1[key] && object2[key]
+      && _.isPlainObject(object1[key]) && _.isPlainObject(object2[key])
+    ) {
       result[key] = makeAst(object1[key], object2[key]);
-    } else if (object1[key] === object2[key]) { // значение ключа совпадают в обих файлах
-      result[key] = { status: 'equal', oldValue: object1[key], newValue: object2[key] };
-    } else if (_.findIndex(keys2, (el) => el === key) === (-1)) { // ключа нет во втором файле
-      result[key] = { status: 'removed', oldValue: object1[key], newValue: '' };
-    } else { // значеие ключа отличается в файлах
-      result[key] = { status: 'updated', oldValue: object1[key], newValue: object2[key] };
-    }
-  });
-
-  keys2.forEach((key) => {
-    if (_.findIndex(keys1, (el) => el === key) === (-1)) { // ключа нет в первом файле
-      result[key] = { status: 'added', oldValue: '', newValue: object2[key] };
+    } else if (object1[key] === object2[key]) {
+      // значение ключа совпадают в обих файлах
+      result[key] = {
+        status: 'equal',
+        oldValue: object1[key],
+        newValue: object2[key],
+      };
+    } else if (!_.includes(keysOfObject2, key)) {
+      // ключа нет во втором файле
+      result[key] = {
+        status: 'removed',
+        oldValue: object1[key],
+        newValue: '',
+      };
+    } else if (!_.includes(keysOfObject1, key)) {
+      // ключа нет в первом файле
+      result[key] = {
+        status: 'added',
+        oldValue: '',
+        newValue: object2[key],
+      };
+    } else {
+      // значеие ключа отличается в файлах
+      result[key] = {
+        status: 'updated',
+        oldValue: object1[key],
+        newValue: object2[key],
+      };
     }
   });
 
@@ -51,16 +60,26 @@ const makeAst = (object1, object2) => {
 };
 
 const gendiff = (filepath1, filepath2, formatName) => {
+  const fullPath1 = path.resolve(process.cwd(), filepath1);
+  const fullPath2 = path.resolve(process.cwd(), filepath2);
+
   try {
-    const object1 = getObject(getFileFullPath(filepath1));
-    const object2 = getObject(getFileFullPath(filepath2));
+    const data1 = fs.readFileSync(fullPath1, 'utf8');
+    const data2 = fs.readFileSync(fullPath2, 'utf8');
+
+    const fileFormat1 = path.extname(fullPath1);
+    const fileFormat2 = path.extname(fullPath2);
+
+    const object1 = parseData(data1, fileFormat1);
+    const object2 = parseData(data2, fileFormat2);
 
     const ast = makeAst(object1, object2);
     return format(ast, formatName);
   } catch (e) {
     console.log('Что-то пошло не так!');
-    console.error(`${e.name}:${e.message}`);
+    console.log(e);
   }
+
   return -1;
 };
 
