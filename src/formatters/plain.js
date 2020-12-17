98d936a72111bfe83cea342ebcd6feadb8da57f6
Sergey Kirillov <s_kirillov@rambler.ru>
2020-12-17 15:29:50 +0300

    refactoring plain formater

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,29 +1,29 @@
+const newPath = (value, path) => (path === ''
+  ? value
+  : `${path}.${value}`);
+
+const toString = (value) => {
+  if (value && typeof value === 'object') {
+    return '[complex value]';
+  }
+  return (typeof (value) === 'string' ? `'${value}'` : value);
+};
+
 const iterAst = (ast, path = '') => {
-  const newPath = (value) => (path === ''
-    ? value
-    : `${path}.${value}`);
-
-  const toString = (value) => {
-    if (value && typeof value === 'object') {
-      return '[complex value]';
-    }
-    return (typeof (value) === 'string' ? `'${value}'` : value);
-  };
-
   const res = ast.map((node) => {
     if (node.type === 'nested') {
-      return iterAst(node.children, newPath(node.name));
+      return iterAst(node.children, newPath(node.name, path));
     }
 
     switch (node.type) {
       case 'equal':
         return '';
       case 'added':
-        return `Property '${newPath(node.name)}' was added with value: ${toString(node.value)}`;
+        return `Property '${newPath(node.name, path)}' was added with value: ${toString(node.value)}`;
       case 'removed':
-        return `Property '${newPath(node.name)}' was removed`;
+        return `Property '${newPath(node.name, path)}' was removed`;
       case 'updated':
-        return `Property '${newPath(node.name)}' was updated. From ${toString(node.oldValue)} to ${toString(node.newValue)}`;
+        return `Property '${newPath(node.name, path)}' was updated. From ${toString(node.oldValue)} to ${toString(node.newValue)}`;
       default:
         throw new Error(`Unkown node type: '${node.type}'`);
     }
