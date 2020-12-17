30aa4a9ad0587fd3b2dc78d909db8fe1cc1847a9
Sergey Kirillov <s_kirillov@rambler.ru>
2020-12-17 13:29:06 +0300

    the order of checks has been changed

--- src/index.js
+++ src/index.js
@@ -15,20 +15,6 @@ const makeAst = (object1, object2) => {
   const keys = _.sortBy(_.union(_.keys(object1), _.keys(object2)));
 
   return keys.map((key) => {
-    if (_.isPlainObject(object1[key]) && _.isPlainObject(object2[key])) {
-      return {
-        name: key,
-        type: 'nested',
-        children: makeAst(object1[key], object2[key]),
-      };
-    }
-    if (_.isEqual(object1[key], object2[key])) {
-      return {
-        name: key,
-        type: 'equal',
-        value: object1[key],
-      };
-    }
     if (!_.has(object2, key)) {
       return {
         name: key,
@@ -41,6 +27,20 @@ const makeAst = (object1, object2) => {
         name: key,
         type: 'added',
         value: object2[key],
+      };
+    }
+    if (_.isPlainObject(object1[key]) && _.isPlainObject(object2[key])) {
+      return {
+        name: key,
+        type: 'nested',
+        children: makeAst(object1[key], object2[key]),
+      };
+    }
+    if (_.isEqual(object1[key], object2[key])) {
+      return {
+        name: key,
+        type: 'equal',
+        value: object1[key],
       };
     }
     return {
