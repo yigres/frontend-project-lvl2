aa3db259fa9418e8924e137452a5f9ab9ab9c4aa
Sergey Kirillov <s_kirillov@rambler.ru>
2020-12-17 11:34:02 +0300

    added default format value

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,7 +2,7 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const format = (ast, outFormat) => {
+const format = (ast, outFormat = 'stylish') => {
   switch (outFormat) {
     case 'stylish': return stylish(ast);
     case 'plain': return plain(ast);
